b134dffb9b59c33ec97d940ed52cb4c2b6b1f595
katerina-kossler <55679304+katerina-kossler@users.noreply.github.com>
2019-10-20 19:56:47 -0700

    check user input errors with cleaner syntax

--- calculator-2/calculator.py
+++ calculator-2/calculator.py
@@ -13,18 +13,25 @@ inputs_lst = ["+", "-", "*", "**", "/", "%"]
 while True:
     calculation = input("select action, num1 and num2 >")
     inputs = calculation.split(" ")
+    operand = inputs[0]
 
-    if len(inputs) < 3:
+    too_long = len(inputs) > 3 
+    too_short = len(inputs) < 3 
+    not_a_number = not inputs[1].isdigit() or not inputs[2].isdigit()
+
+    if operand == 'q':
+            break
+
+    elif (too_long 
+        or too_short 
+        or not_a_number):
         print("Invalid input, ex. + 1 2. Try again!")
+
     else:
-        operand = inputs[0]
         num1 = float(inputs[1])
         num2 = float(inputs[2])
         
-        if operand == 'q':
-            break
-        
-        elif operand not in inputs_lst:
+        if operand not in inputs_lst:
             print("Invalid, try again")
 
         elif operand == "+":
