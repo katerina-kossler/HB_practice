0b6a5ebf99893db0af17f680b98edf38e3c8eea2
Katerina Kossler <katerinakossler@gmail.com>
2019-12-08 21:08:18 -0800

    Added project searching and links between projects, students, & project grades

--- project-tracker-flask/hackbright_web.py
+++ project-tracker-flask/hackbright_web.py
@@ -1,11 +1,13 @@
 """A web application for tracking projects, students, and student grades."""
 
 from flask import Flask, request, render_template
-
 import hackbright
 
 app = Flask(__name__)
 
+# student-search > student
+# new-student > student-added (> student)
+# project-search > project
 
 @app.route("/student-search")
 def get_student_form():
@@ -21,7 +23,6 @@ def get_student():
     github = request.args.get('github')
     first, last, github = hackbright.get_student_by_github(github)
     graded_projects = hackbright.get_grades_by_github(github)
-
     html = render_template("student_info.html",
                            first=first,
                            last=last,
@@ -46,9 +47,34 @@ def student_add():
     last_name = request.form.get('last_name')
     github = request.form.get('github')
     hackbright.make_new_student(first_name, last_name, github)
-
-    return render_template("student_added.html",
+    html = render_template("student_added.html",
                            github=github)
+
+    return html
+
+
+@app.route("/project-search")
+def get_project_form():
+    """Searches for project info based on project title"""
+    
+    return render_template('project_search.html')
+
+
+@app.route("/project", methods=['GET'])
+def get_project():
+    """Gets project information from the database"""
+
+    title = request.args.get('title')
+    project, description, max_grade = hackbright.get_project_by_title(title)
+    grades = hackbright.get_grades_by_title(title)
+    html = render_template('project_info.html', 
+                           title=title,
+                           description=description,
+                           max_grade=max_grade,
+                           grades=grades)
+
+    return html
+
 
 
 if __name__ == "__main__":
