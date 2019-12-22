f99f38017083a3f0db5eaf380860fd88fd509a15
Katerina Kossler <katerinakossler@gmail.com>
2019-12-21 17:26:33 -0800

    expand ratings database and develop basic webapp to display current movies and users in db; allow to add new users and login functionality

--- ratings/server.py
+++ ratings/server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Movie, Rating
 
 
 app = Flask(__name__)
@@ -22,8 +22,78 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template('homepage.html')
 
+
+@app.route('/users')
+def user_list():
+    """Show list of users"""
+
+    users = User.query.all()
+    return render_template("user_list.html",
+                            users=users)
+
+
+@app.route('/movies')
+def movie_list():
+    """Show list of movies"""
+
+    movies = Movie.query.all()
+    return render_template('movie_list.html',
+                            movies=movies)
+
+
+@app.route('/register', methods=['GET'])
+def display_register_form():
+    """Shows a form for user to add information"""
+
+    return render_template('register_form.html')
+
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Processes user information"""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+    age = int(request.form.get("age"))
+    zipcode = request.form.get("zipcode")
+
+    user_in_system = User.query.filter_by(email=email).first()
+
+    if not user_in_system:
+        user = User(email=email,
+                    password=password,
+                    age=age,
+                    zipcode=zipcode)
+        db.session.add(user)
+        db.session.commit()
+
+    return redirect('/')
+
+
+@app.route('/login', methods=['GET'])
+def display_login_form():
+    """Shows the login screen that requests user email and password"""
+
+    return render_template('login_form.html')
+
+
+@app.route('/login', methods=['POST'])
+def authenticate_user():
+    """Compare user input information to stored users"""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user_in_system = User.query.filter_by(email=email).first()
+    password_in_system = user_in_system.password
+
+    if (password_in_system == password):
+        session['current_user'] = user_in_system.user_id
+        flash('Successfully Logged in')
+
+    return redirect('/')
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
